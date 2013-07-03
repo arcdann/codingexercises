08dddd7143734ba805751bcf0a5328f2a7259d78
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-07-03 23:44:38 +0400

    Dimension

--- minesweeper/com/daniloff/minesweeper/Bootstrap.java
+++ minesweeper/com/daniloff/minesweeper/Bootstrap.java
@@ -1,11 +1,16 @@
 package com.daniloff.minesweeper;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.daniloff.minesweeper.invite.InviteDialog;
 
 public class Bootstrap {
 
 	public static void main(String[] args) {
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		InviteDialog dialog = new InviteDialog();
+		dialog.setScreen(screen);
 		dialog.init();
 	}
 }
