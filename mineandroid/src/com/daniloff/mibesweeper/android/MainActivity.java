6b0694052c7a00e8c0df52461b9ea0c756817540
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-07-16 00:33:55 +0400

    add views

--- mineandroid/src/com/daniloff/mibesweeper/android/MainActivity.java
+++ mineandroid/src/com/daniloff/mibesweeper/android/MainActivity.java
@@ -4,16 +4,37 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	private final int X=10;
+	private final int Y=10;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		TextView textView1 = (TextView) findViewById(R.id.textView1);
-		textView1.setText("new text");
+		TextView topText = (TextView) findViewById(R.id.topText);
+		topText.setText("Minesweeper Android top text");
+		
+	TableLayout tl = (TableLayout) findViewById(R.id.tableLayout);
+
+	for (int y=0;y<Y;y++){
+		TableRow tr = new TableRow(this);
+		tr.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,Y));
+		for(int x=0;x<X;x++){
+			Button button = new Button(this);
+			button.setText(x+" "+y);
+			tr.addView(button);
+		}
+		tl.addView(tr);
+	}
+		
+
 	}
 
 	@Override
