e77b7d2711bc31538c29b0e5d34bc9933feea5ec
Arc.B.Daniloff <arc.b.daniloff@gmail.com>
2013-07-17 23:11:46 +0400

    activity listener

--- mineandroid/src/com/daniloff/mibesweeper/android/MainActivity.java
+++ mineandroid/src/com/daniloff/mibesweeper/android/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.TextView;
 import com.daniloff.minesweeper.client.field.model.MineFieldLogic;
 import com.daniloff.minesweeper.client.settings.GameSettings;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	private final int X = 10;
 	private final int Y = 10;
@@ -47,6 +47,41 @@ public class MainActivity extends Activity {
 
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	// listen control buttons
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.newGameButton:
+			topText.setText("New game button pressed");
+			break;
+		case R.id.restartButton:
+			topText.setText("Restart button pressed");
+			break;
+		case R.id.pauseButton:
+			topText.setText("Pause button pressed");
+			break;
+		case R.id.flagButton:
+			if (!flagButtonPressed) {
+				topText.setText("Flag button pressed");
+				flagButtonPressed = true;
+				v.setBackgroundColor(Color.RED);
+			} else {
+				topText.setText("Flag button unpressed");
+				flagButtonPressed = false;
+				v.setBackgroundColor(Color.CYAN);
+			}
+			break;
+		}
+	}
+
+	// listen play buttons
 	private void listenButton() {
 		listener = new OnClickListener() {
 
@@ -54,14 +89,24 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				int id = v.getId();
 				int x = id % 100;
-				int y = (id - x)/100;
-				topText.setText("buttons[" + x + "][" + y + "] stepped");
-//				v.setBackgroundColor(0);
-				buttons[x][y].setText("#");
-//				buttons[x][y].setBackgroundColor(Color.BLUE);
+				int y = (id - x) / 100;
+				if (!flagButtonPressed) {
+					topText.setText("buttons[" + x + "][" + y + "] stepped");
+					// v.setBackgroundColor(Color.BLUE);
+					buttons[x][y].setText("#");
+
+					// field.step(xInner, yInner);
+					// String stubText = field.stub();
+					// topText.setText(stubText);
+
+				} else {
+					topText.setText("buttons[" + x + "][" + y + "] flagged");
+					buttons[x][y].setText("F");
+					flagButtonPressed = false;
+					findViewById(R.id.flagButton).setBackgroundColor(Color.CYAN);
+				}
 			}
 		};
-
 	}
 
 	public void drawMineField() {
@@ -77,36 +122,10 @@ public class MainActivity extends Activity {
 
 				buttons[x][y].setWidth(screenWidth / X - 5);
 				buttons[x][y].setHeight(buttons[x][y].getWidth());
-//				buttons[x][y].setBackgroundColor(Color.GREEN);
-				final int xInner = x;
-				final int yInner = y;
-
+				// buttons[x][y].setBackgroundColor(Color.GREEN);
 				int id = y * 100 + x;
 				buttons[x][y].setId(id);
 				buttons[x][y].setOnClickListener(listener);
-
-				// buttons[x][y].setOnClickListener(new OnClickListener() {
-				// @Override
-				// public void onClick(View v) {
-				// if (!flagButtonPressed) {
-				// topText.setText("buttons[" + xInner + "][" + yInner +
-				// "] stepped");
-				// buttons[xInner][yInner].setText("#");
-				//
-				// // field.step(xInner, yInner);
-				//
-				// // String stubText = field.stub();
-				//
-				// // topText.setText(stubText);
-				//
-				// } else {
-				// topText.setText("buttons[" + xInner + "][" + yInner +
-				// "] flagged");
-				// flagButtonPressed = false;
-				// buttons[xInner][yInner].setText("F");
-				// }
-				// }
-				// });
 				tr.addView(buttons[x][y]);
 			}
 			tl.addView(tr);
@@ -114,46 +133,21 @@ public class MainActivity extends Activity {
 
 		Button newGameButton = (Button) findViewById(R.id.newGameButton);
 		newGameButton.setText("New game");
-		newGameButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				topText.setText("New game button pressed");
-			}
-		});
+		newGameButton.setOnClickListener(this);
 
 		Button restartButton = (Button) findViewById(R.id.restartButton);
 		restartButton.setText("Restart");
 		restartButton.setBackgroundColor(Color.BLUE);
-		restartButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				topText.setText("Restart button pressed");
-			}
-		});
+		restartButton.setOnClickListener(this);
 
 		Button pauseButton = (Button) findViewById(R.id.pauseButton);
 		pauseButton.setText("Pause");
-		pauseButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				topText.setText("Pause button pressed");
-			}
-		});
+		pauseButton.setOnClickListener(this);
 
 		Button flagButton = (Button) findViewById(R.id.flagButton);
 		flagButton.setText("Flag");
-		flagButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (!flagButtonPressed) {
-					topText.setText("Flag button pressed");
-					flagButtonPressed = true;
-				} else {
-					topText.setText("Flag button unpressed");
-					flagButtonPressed = false;
-				}
-			}
-		});
+		flagButton.setBackgroundColor(Color.CYAN);
+		flagButton.setOnClickListener(this);
 
 	}
 
@@ -172,14 +166,6 @@ public class MainActivity extends Activity {
 				}
 			}
 		}
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 }
